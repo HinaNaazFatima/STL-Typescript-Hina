6b19c9ba9b8eaa79ad7a1efa904953013a89467a
softwaretestlab <softwaretestlab@hotmail.com>
2025-06-16 17:24:43 +1000

    envupdate

--- tests/BasicsTypeScript/TS12_TagUsage.spec.ts
+++ tests/BasicsTypeScript/TS12_TagUsage.spec.ts
@@ -17,6 +17,7 @@ test.describe('Login Tests', () => {
     const errorMessage = page.locator('[data-test="error"]'); 
     await expect(errorMessage).toBeVisible();
     await expect(errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
+    
   });
 });
 
